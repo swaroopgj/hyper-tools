bccefcd45cb62e172d217caac6238e9e19013421
andrewheusser <andrew.heusser@gmail.com>
2016-12-10 19:15:06 -0500

    describe align branch

--- python/hypertools/describe.py
+++ python/hypertools/describe.py
@@ -1,46 +1,45 @@
 #!/usr/bin/env python
 
-"""
-Correlates raw data with PCA reduced data to get a sense for how well the data
-can be summarized with n dimensions.  Useful for evaluating quality of PCA reduced
-plots.
-
-INPUTS:
--numpy array(s)
--list of numpy arrays
-
-OUTPUTS:
--Dictionary with correlation values between raw data and PCA reduced data (optional)
-"""
-
-##PACKAGES##
 from __future__ import division
-import warnings
 import numpy as np
 from scipy.spatial.distance import pdist
 from scipy.spatial.distance import cdist
 from .align import *
 from .helpers import reduceD, reduceD_list
 import scipy.spatial.distance as sd
-import matplotlib.pyplot as plt
+
 import seaborn as sns
 sns.set(style="darkgrid")
 
-##MAIN FUNCTION##
-def describe(x, return_data=False):
-    warnings.warn('When input data is large, this computation can take a long time.')
+def describe(x, include=[None]):
 
     ##SUB FUNCTIONS##
-    def PCA_summary(x,max_dims):
+
+    def align_summary(x):
+
+        if type(x) is not list:
+            print('Must pass a list of arrays.')
+
+        def compute_a(x):
+            dists= []
+            for xidx,xi in enumerate(x):
+                x_cov = 1 - pdist(xi,'correlation')
+                y_cov = 1 - pdist(np.mean(np.array([yi for yidx,yi in enumerate(x) if yidx!=xidx]),0),'correlation')
+                dists.append(sd.cdist(sd.squareform(x_cov), sd.squareform(y_cov), 'cosine'))
+            return np.mean(dists)
+
+        before = compute_a(x)
+        x_aligned = align(x)
+        after = compute_a(x_aligned)
+
+        return 1 - np.exp(-((before / after) - 1))
+
+    def PCA_summary(x):
         if type(x) is list:
             x = np.vstack(x)
         cov_alldims = pdist(x,'correlation')
-        corrs=[]
-        for num in range(2,max_dims):
-            cov_PCA = pdist(reduceD(x,num),'correlation')
-            corrs.append(np.corrcoef(cov_alldims, cov_PCA)[0][1])
-            del cov_PCA
-        return corrs
+        cov_PCA =  [(pdist(reduceD(x,num),'correlation')) for num in range(2,x.shape[1]+1)]
+        return [np.corrcoef(cov_alldims, cov_PCA_i)[0][1] for cov_PCA_i in cov_PCA]
 
     if type(x) is list:
         pass
@@ -48,17 +47,13 @@ def describe(x, return_data=False):
         x = [x]
 
     attrs = {}
-    attrs['PCA_summary'] = {}
-    attrs['PCA_summary']['average'] = PCA_summary(x,x[0].shape[1])
-    max_group = np.where(attrs['PCA_summary']['average']==np.max(attrs['PCA_summary']['average']))[0][0]
-    attrs['PCA_summary']['individual'] = [PCA_summary(x_i,max_group) for x_i in x]
 
-    fig, ax = plt.subplots()
-    ax = sns.tsplot(attrs['PCA_summary']['individual'], err_style="unit_traces")
-    ax.set_title('Correlation with raw data by number of PCA components')
-    ax.set_ylabel('Correlation')
-    ax.set_xlabel('Number of PCA components')
-    plt.show()
+    if 'PCA' in include or include==[None]:
+        attrs['PCA_summary'] = {}
+        attrs['PCA_summary']['average'] = PCA_summary(x)
+        attrs['PCA_summary']['individual'] = [PCA_summary(x_i) for x_i in x]
 
-    if return_data==True:
-        return attrs
+    if 'align' in include or include==[None]:
+        attrs['Align_summary'] = [align_summary([reduceD(i,num) for i in x]) for num in range(2,10)]
+
+    return attrs
